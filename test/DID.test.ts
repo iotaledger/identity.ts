035472b63714ee796b2b6d954cef6272d68ce478
LisaSchneider94 <lisa.schneider@iota.org>
2020-01-28 16:36:11 +0100

    clean up tests

---
+++ test/DID.test.ts
@@ -0,0 +1,180 @@
+import { expect } from 'chai';
+import 'mocha';
+import { CreateRandomDID } from '../src/Helpers/CreateRandomDID';
+import { GenerateRSAKeypair, GenerateECDSAKeypair } from '../src/Helpers/GenerateKeypair';
+import { RSAKeypair } from '../src/Encryption/RSAKeypair';
+import { ECDSAKeypair } from '../src/Encryption/ECDSAKeypair';
+import { DID } from '../src/DID/DID';
+import { Hash } from '../src/Encryption/Hash';
+import { DIDDocument } from '../src/DID/DIDDocument';
+import { DIDPublisher } from '../src/IOTA/DIDPublisher';
+import { GenerateSeed } from '../src/Helpers/GenerateSeed';
+import { Service } from '../src/DID/Service';
+
+
+//const provider : string = "https://nodes.devnet.iota.org:443";
+const provider : string = "https://nodes.thetangle.org:443 "
+
+describe('DID Functionalities with RSA', function() {
+    let uuid : string;
+    it('Should create a valid DID from a UUID', async function() {
+        let keypair : RSAKeypair = await GenerateRSAKeypair();
+        uuid = Hash(keypair.GetPublicKey());
+        expect("did:iota:main:"+uuid, new DID(uuid).GetSpecificDID());
+    });
+
+    it('Should create a valid DID from a did:method:uuid', async function() {
+        expect("did:iota:main:"+uuid, new DID("did:iota:"+uuid).GetSpecificDID());
+    });
+
+    it('Should create a valid DID from a did:method:network:uuid', async function() {
+        expect("did:iota:dev:"+uuid, new DID("did:iota:dev:"+uuid).GetSpecificDID());
+    });
+
+    it('Should create a valid DID from a did:method:network:uuid#fragment', async function() {
+        let did : DID = new DID("did:iota:dev:"+uuid + "#fragment");
+        expect("did:iota:dev:"+uuid, did.GetSpecificDID());
+        expect("fragment", did.GetFragment());
+    });
+
+});
+
+
+describe('DID Document with RSA', function() {
+    let document : DIDDocument;
+    let seed : string = GenerateSeed();
+    let root : string;
+    let documentFromTangle : DIDDocument;
+    let publisher : DIDPublisher;
+    let service : Service;
+
+    it('Should create and output a valid DID Document', async function(){
+        document = await CreateRandomDID(seed);
+        let keypair : RSAKeypair = await GenerateRSAKeypair();
+        document.AddKeypair(keypair, "keys-1");
+        expect(document.GetJSONDIDDocument()).to.not.be.undefined;
+    });
+
+    it('Should publish the DID Document', async function() {
+        this.timeout(60000);
+        publisher = new DIDPublisher(provider, seed);
+        root = await publisher.PublishDIDDocument(document, "DIDTEST", 14)
+        expect(root).to.not.be.undefined;
+    });
+
+    it('Should read the same document from the Tangle', async function() {
+        this.timeout(60000);
+        await delay(2000); //Sleep prevents the node to not know about the first tx yet, failing the test.
+        documentFromTangle = await DIDDocument.readDIDDocument(provider, root);
+        expect(documentFromTangle.GetJSONDIDDocument()).to.deep.equal(document.GetJSONDIDDocument());
+    });
+
+    it('Should handle empty DID Documents', async function() {
+        const result = await DIDDocument.readDIDDocument(provider, GenerateSeed(81));
+    });
+
+    it('Should Sign locally and Verify from loaded DID Document', async function() {
+        let msg : string = "Hello World";
+        let signature : Buffer = await document.GetKeypair("keys-1").GetEncryptionKeypair().Sign(msg);
+        expect(await documentFromTangle.GetKeypair("keys-1").GetEncryptionKeypair().Verify(msg, signature)).to.be.true;
+    });
+
+    it('Should add a ServiceEndpoint', function() {
+        service = new Service(document.GetDID(), "test", "TestService", GenerateSeed());
+        document.AddServiceEndpoint(service);
+        expect(document.GetService("test")).to.not.be.null;
+    });
+
+    it('Should update the DIDDocument correctly and contain a ServiceEndpoint', async function() {
+        this.timeout(60000);	
+        await publisher.PublishDIDDocument(document, "DIDTEST", 14);
+        documentFromTangle = await DIDDocument.readDIDDocument(provider, root);
+        expect(documentFromTangle.GetJSONDIDDocument()).to.deep.equal(document.GetJSONDIDDocument());
+        expect(documentFromTangle.GetService("test").EncodeToJSON()).to.deep.equal(service.EncodeToJSON());
+    });
+});
+
+describe('DID Functionalities with ECDSA', function() {
+    let uuid : string;
+    it('Should create a valid DID from a UUID', async function() {
+        let keypair : ECDSAKeypair = await GenerateECDSAKeypair();
+        uuid = Hash(keypair.GetPublicKey());
+        expect("did:iota:main:"+uuid, new DID(uuid).GetSpecificDID());
+    });
+
+    it('Should create a valid DID from a did:method:uuid', async function() {
+        expect("did:iota:main:"+uuid, new DID("did:iota:"+uuid).GetSpecificDID());
+    });
+
+    it('Should create a valid DID from a did:method:network:uuid', async function() {
+        expect("did:iota:dev:"+uuid, new DID("did:iota:dev:"+uuid).GetSpecificDID());
+    });
+
+    it('Should create a valid DID from a did:method:network:uuid#fragment', async function() {
+        let did : DID = new DID("did:iota:dev:"+uuid + "#fragment");
+        expect("did:iota:dev:"+uuid, did.GetSpecificDID());
+        expect("fragment", did.GetFragment());
+    });
+
+});
+
+
+describe('DID Document with ECDSA', function() {
+    let document : DIDDocument;
+    let seed : string = GenerateSeed();
+    let root : string;
+    let documentFromTangle : DIDDocument;
+    let publisher : DIDPublisher;
+    let service : Service;
+
+    it('Should create and output a valid DID Document', async function(){
+        document = await CreateRandomDID(seed);
+        let keypair : ECDSAKeypair = await GenerateECDSAKeypair();
+        document.AddKeypair(keypair, "keys-1");
+        expect(document.GetJSONDIDDocument()).to.not.be.undefined;
+    });
+
+    it('Should publish the DID Document', async function() {
+        this.timeout(40000);
+        publisher = new DIDPublisher(provider, seed);
+        root = await publisher.PublishDIDDocument(document, "DIDTEST", 14)
+        expect(root).to.not.be.undefined;
+    });
+
+    it('Should read the same document from the Tangle', async function() {
+        this.timeout(40000);
+        await delay(2000); //Sleep prevents the node to not know about the first tx yet, failing the test.
+        documentFromTangle = await DIDDocument.readDIDDocument(provider, root);
+        expect(documentFromTangle.GetJSONDIDDocument()).to.deep.equal(document.GetJSONDIDDocument());
+    });
+
+    it('Should handle empty DID Documents', async function() {
+        const result = await DIDDocument.readDIDDocument(provider, GenerateSeed(81));
+    });
+
+    it('Should Sign locally and Verify from loaded DID Document', async function() {
+        let msg : string = "Hello World";
+        let signature : Buffer = await document.GetKeypair("keys-1").GetEncryptionKeypair().Sign(msg);
+        expect(await documentFromTangle.GetKeypair("keys-1").GetEncryptionKeypair().Verify(msg, signature)).to.be.true;
+    });
+
+    it('Should add a ServiceEndpoint', function() {
+        service = new Service(document.GetDID(), "test", "TestService", GenerateSeed());
+        document.AddServiceEndpoint(service);
+        expect(document.GetService("test")).to.not.be.null;
+    });
+
+    it('Should update the DIDDocument correctly and contain a ServiceEndpoint', async function() {
+        this.timeout(40000);	
+        await publisher.PublishDIDDocument(document, "DIDTEST", 14);
+        documentFromTangle = await DIDDocument.readDIDDocument(provider, root);
+        expect(documentFromTangle.GetJSONDIDDocument()).to.deep.equal(document.GetJSONDIDDocument());
+        expect(documentFromTangle.GetService("test").EncodeToJSON()).to.deep.equal(service.EncodeToJSON());
+    });
+});
+
+
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
---
+++ test/VC.test.ts
@@ -0,0 +1,361 @@
+import { expect } from 'chai';
+import 'mocha';
+import { Credential } from '../src/VC/Credential';
+import { VerifiableCredential } from '../src/VC/VerifiableCredential';
+import { SchemaManager } from '../src/VC/SchemaManager';
+import { Schema } from '../src/VC/Schema';
+import { DID, DIDDocument, SignDIDAuthentication, VerifyDIDAuthentication, BuildRSAProof } from '../src';
+import { CreateRandomDID } from '../src/Helpers/CreateRandomDID';
+import { DIDPublisher } from '../src/IOTA/DIDPublisher';
+import { GenerateSeed } from '../src/Helpers/GenerateSeed';
+import { Presentation } from '../src/VC/Presentation';
+import { VerifiablePresentation } from '../src/VC/VerifiablePresentation';
+import { VerificationErrorCodes } from '../src/VC/VerifiableObject';
+import { Proof, ProofBuildingMethod, ProofParameters } from '../src/VC/Proof/Proof';
+import { ProofTypeManager } from '../src/VC/Proof/ProofTypeManager';
+import { DecodeProofDocument } from '../src/Helpers/DecodeProofDocument';
+import { GenerateRSAKeypair } from '../src/Helpers/GenerateKeypair';
+import { RSAKeypair } from '../src/Encryption/RSAKeypair';
+import { ECDSAKeypair } from '../src/Encryption/ECDSAKeypair';
+import { BuildECDSAProof } from '../src/VC/Proof/ECDSAProof';
+import { GenerateECDSAKeypair } from '../src/Helpers/GenerateKeypair';
+
+//const provider : string = "https://nodes.devnet.iota.org:443";
+const provider : string = "https://nodes.thetangle.org:443 "
+
+let RandomDID : DID = new DID("did:iota:main:ABCABCABC");
+
+describe('Schemas', function() {
+    let schema : Schema;
+    let testObject : {};
+    
+    let SchemaList : string[] = ['DIDAuthenticationCredential', 'DomainValidatedCertificate', 'WhiteListedCredential'];
+    it('Should contain a list of default schemas', function() {
+        expect(SchemaManager.GetInstance().GetSchemaNames()).to.deep.equal(SchemaList);
+    });
+    
+    it('Should be able to add an extra schema', function() {
+        //Add timeout 
+        this.timeout(4000);
+        SchemaList.push("IOTATrainingCertificate");
+        SchemaManager.GetInstance().AddSchema("IOTATrainingCertificate", {
+            type : "object",
+            required : ["trainingTitle", "participant", "participationDate"],
+            properties : {
+                "trainingTitle" : { 
+                    type : "string"
+                },
+                "participant" : { 
+                    type : "string"
+                },
+                "participationDate" : { 
+                    type : "string"
+                }
+            }
+        });
+        expect(SchemaManager.GetInstance().GetSchemaNames()).to.deep.equal(SchemaList);
+    });
+
+    it('Should validate correctly', function() {
+        schema = SchemaManager.GetInstance().GetSchema("IOTATrainingCertificate");
+        testObject = {
+            "trainingTitle" : "IOTA Developer Training",
+            "participant" : "Jelly von Yellowburg",
+            "participationDate" : new Date().toUTCString()
+        };
+        expect(schema.DoesObjectFollowSchema(testObject)).to.be.true;
+    });
+
+    it('Should validate with extra fields', function() {
+        let testObject2 : {} = { ...testObject, ...{"ExtraField" : "Hello World"}};
+        expect(schema.DoesObjectFollowSchema(testObject2)).to.be.true;
+    });
+
+    it('Should fail with a missing field', function() {
+        testObject = {
+            "trainingTitle" : "IOTA Developer Training",
+            "participant" : "Jelly von Yellowburg"
+        };
+        expect(schema.DoesObjectFollowSchema(testObject)).to.be.false;
+    });
+
+    it('Should fail with a wrong type', function() {
+        testObject = {
+            "trainingTitle" : "IOTA Developer Training",
+            "participant" : "Jelly von Yellowburg",
+            "participationDate" : 12
+        };
+        expect(schema.DoesObjectFollowSchema(testObject)).to.be.false;
+    });
+
+    it('Should correctly verify DIDAuthenticationCredential', function() {
+        schema = SchemaManager.GetInstance().GetSchema("DIDAuthenticationCredential");
+        testObject = {
+            "DID" : "did:iota:main:123123"
+        }
+        expect(schema.DoesObjectFollowSchema(testObject)).to.be.true;
+    });
+
+    it('Should correctly verify DomainValidatedCertificate', function() {
+        schema = SchemaManager.GetInstance().GetSchema("DomainValidatedCertificate");
+        testObject = {
+            "id" : "did:iota:main:123123",
+            "domains" : ["*.iota.org", "*.reddit.com"]
+        }
+        expect(schema.DoesObjectFollowSchema(testObject)).to.be.true;
+    });
+
+    it('Should not trust a random DID', function() {
+        expect(schema.IsDIDTrusted(RandomDID)).to.be.false;
+    });
+
+    it('Should add and trust a DID', function() {
+        schema.AddTrustedDID(RandomDID);
+        expect(schema.IsDIDTrusted(RandomDID)).to.be.true;
+    })
+});
+
+describe('RSA Verifiable Credentials', async function() {    
+    let IssuerDIDDocument : DIDDocument;
+    let issuerSeed : string;
+    let issuerPrivateKey : string;
+    let SubjectDIDDocument : DIDDocument;
+    let subjectSeed : string;
+
+    let credential : Credential;
+    let verifiableCredential : VerifiableCredential;
+    let proofMethod : ProofBuildingMethod;
+    let VCProof : Proof;
+    let presentation : Presentation;
+    let verifiablePresentation : VerifiablePresentation;
+    let presentationProof : Proof;
+
+    before(async function() {
+        this.timeout(60000);
+        issuerSeed = GenerateSeed();
+        IssuerDIDDocument = CreateRandomDID(issuerSeed);
+        let keypair : RSAKeypair = await GenerateRSAKeypair();
+        issuerPrivateKey = keypair.GetPrivateKey();
+        IssuerDIDDocument.AddKeypair(keypair, "keys-1");
+        let publisher : DIDPublisher = new DIDPublisher(provider, issuerSeed);
+        await publisher.PublishDIDDocument(IssuerDIDDocument, "DIDTEST", 14);
+        SchemaManager.GetInstance().GetSchema("DomainValidatedCertificate").AddTrustedDID(IssuerDIDDocument.GetDID());
+        subjectSeed = GenerateSeed();
+        SubjectDIDDocument = CreateRandomDID(subjectSeed);
+        let keypair2 : RSAKeypair = await GenerateRSAKeypair();
+        SubjectDIDDocument.AddKeypair(keypair2, "keys-1");
+        let publisher2 : DIDPublisher = new DIDPublisher(provider, subjectSeed);
+        await publisher2.PublishDIDDocument(SubjectDIDDocument, "DIDTEST", 14);
+        proofMethod = ProofTypeManager.GetInstance().GetProofBuilder("RsaSignature2018");
+    });
+
+    it('Should be able to a credential', function() {
+        let domainCertificate = {
+            id : SubjectDIDDocument.GetDID().GetDID(),
+            domains : [
+                "blog.iota.org",
+                "coordicide.iota.org",
+                "docs.iota.org"
+            ]
+        };
+        credential = Credential.Create(SchemaManager.GetInstance().GetSchema("DomainValidatedCertificate"), IssuerDIDDocument.GetDID(), domainCertificate);
+        expect(credential.GetCredential()).to.not.be.undefined;
+    });
+
+    it('Should be able to Encode / Decode a credential to be the same', function() {
+        let importedCredential : Credential = Credential.DecodeFromJSON(credential.EncodeToJSON());
+        expect(importedCredential.EncodeToJSON()).to.deep.equal(credential.EncodeToJSON());
+    });
+
+    it('Should be able to create, sign and verify a Verifiable Credential' , function() {
+        VCProof = proofMethod({'issuer' : IssuerDIDDocument, 'issuerKeyId' : "keys-1"});
+        VCProof.Sign(credential.EncodeToJSON());
+        verifiableCredential = VerifiableCredential.Create(credential, VCProof);
+        expect(verifiableCredential.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+    });
+
+    it('Should be able to Encode / Decode a Verifiable Credential and still verify', async function() {
+        this.timeout(30000);
+        await delay(2000);
+        let proofParameters : ProofParameters = await DecodeProofDocument(verifiableCredential.EncodeToJSON().proof, provider);
+        let importedVerifiableCredential : VerifiableCredential = VerifiableCredential.DecodeFromJSON(verifiableCredential.EncodeToJSON(), proofParameters);
+        expect(importedVerifiableCredential.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+        expect(importedVerifiableCredential.EncodeToJSON()).to.deep.equal(verifiableCredential.EncodeToJSON());
+    });
+
+    it('Should test all Verification Error codes for Verifiable Credentials', function() {
+        
+    });
+
+    it('Should be able to create a presentation from a Verifiable Credential', function() {
+        presentation = Presentation.Create([verifiableCredential]);
+        expect(presentation.EncodeToJSON().verifiableCredential[0]).to.deep.equal(verifiableCredential.EncodeToJSON());
+    });
+
+    it('Should be able to Encode / Decode a presentation to be the same', async function() {
+        let importPresentation : Presentation = await Presentation.DecodeFromJSON(presentation.EncodeToJSON(), provider);
+        expect(importPresentation.EncodeToJSON()).to.deep.equal(presentation.EncodeToJSON());
+    });
+
+    it('Should be able to create, sign and verify the Verifiable Presentation', function() {
+        presentationProof = proofMethod({'issuer' : SubjectDIDDocument, 'issuerKeyId' : "keys-1", challengeNonce : "123"});
+        presentationProof.Sign(presentation.EncodeToJSON());
+        verifiablePresentation = VerifiablePresentation.Create(presentation, presentationProof);
+        expect(verifiablePresentation.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+    });
+
+    //verifiablePresentation Shouldn't this be enough to integrate into VerifiableObject and do DecodeProofDocument?
+    it('Should be able to Encode / Decode a Verifiable Presentation and still verify', async function() {
+        let proofParameters : ProofParameters = await DecodeProofDocument(verifiablePresentation.EncodeToJSON().proof, provider);
+        let importVerifiablePresentation : VerifiablePresentation = await VerifiablePresentation.DecodeFromJSON(verifiablePresentation.EncodeToJSON(), provider, proofParameters);
+        expect(importVerifiablePresentation.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+        expect(importVerifiablePresentation.EncodeToJSON()).to.deep.equal(verifiablePresentation.EncodeToJSON());
+    });
+
+    it('Should test all Verification Error codes for Verifiable Presentation', function() {
+
+    });
+
+    let DIDAuth : VerifiablePresentation;
+    it('Should create a DID Authentication Verifiable Presentation', function() {
+        const DIDAuthVC = SignDIDAuthentication(SubjectDIDDocument, "keys-1", GenerateSeed(12));
+        const presentation = Presentation.Create([DIDAuthVC]);
+        const presentationProof = BuildRSAProof({issuer:SubjectDIDDocument, issuerKeyId:"keys-1", challengeNonce:GenerateSeed(12)});
+        presentationProof.Sign(presentation.EncodeToJSON());
+        DIDAuth = VerifiablePresentation.Create(presentation, presentationProof);
+        SchemaManager.GetInstance().GetSchema("DIDAuthenticationCredential").AddTrustedDID(SubjectDIDDocument.GetDID());
+        expect(DIDAuth.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+        SchemaManager.GetInstance().GetSchema("DIDAuthenticationCredential").RemoveTrustedDID(SubjectDIDDocument.GetDID());
+        expect(DIDAuth.GetVerifiedTypes()).to.deep.equal(["DIDAuthenticationCredential"]);
+    });
+
+    it('Should be able to verify an imported DID Authentication', async function() {
+        expect(await VerifyDIDAuthentication(DIDAuth.EncodeToJSON(), provider)).to.deep.equal(VerificationErrorCodes.SUCCES);
+    });
+});
+
+
+
+
+describe('ECDSA Verifiable Credentials', async function() {    
+    let IssuerDIDDocument : DIDDocument;
+    let issuerSeed : string;
+    let issuerPrivateKey : string;
+    let SubjectDIDDocument : DIDDocument;
+    let subjectSeed : string;
+
+    let credential : Credential;
+    let verifiableCredential : VerifiableCredential;
+    let proofMethod : ProofBuildingMethod;
+    let VCProof : Proof;
+    let presentation : Presentation;
+    let verifiablePresentation : VerifiablePresentation;
+    let presentationProof : Proof;
+
+    before(async function() {
+        this.timeout(80000);
+        issuerSeed = GenerateSeed();
+        IssuerDIDDocument = CreateRandomDID(issuerSeed);
+        let keypair : ECDSAKeypair = await GenerateECDSAKeypair();
+        issuerPrivateKey = keypair.GetPrivateKey();
+        IssuerDIDDocument.AddKeypair(keypair, "keys-1");
+        let publisher : DIDPublisher = new DIDPublisher(provider, issuerSeed);
+        await publisher.PublishDIDDocument(IssuerDIDDocument, "DIDTEST", 14);
+        SchemaManager.GetInstance().GetSchema("DomainValidatedCertificate").AddTrustedDID(IssuerDIDDocument.GetDID());
+        subjectSeed = GenerateSeed();
+        SubjectDIDDocument = CreateRandomDID(subjectSeed);
+        let keypair2 : ECDSAKeypair = await GenerateECDSAKeypair();
+        SubjectDIDDocument.AddKeypair(keypair2, "keys-1");
+        let publisher2 : DIDPublisher = new DIDPublisher(provider, subjectSeed);
+        await publisher2.PublishDIDDocument(SubjectDIDDocument, "DIDTEST", 14);
+        proofMethod = ProofTypeManager.GetInstance().GetProofBuilder("EcdsaSecp256k1Signature2019");
+    });
+
+    it('Should be able to a credential', function() {
+        let domainCertificate = {
+            id : SubjectDIDDocument.GetDID().GetDID(),
+            domains : [
+                "blog.iota.org",
+                "coordicide.iota.org",
+                "docs.iota.org"
+            ]
+        };
+        credential = Credential.Create(SchemaManager.GetInstance().GetSchema("DomainValidatedCertificate"), IssuerDIDDocument.GetDID(), domainCertificate);
+        expect(credential.GetCredential()).to.not.be.undefined;
+    });
+
+    it('Should be able to Encode / Decode a credential to be the same', function() {
+        let importedCredential : Credential = Credential.DecodeFromJSON(credential.EncodeToJSON());
+        expect(importedCredential.EncodeToJSON()).to.deep.equal(credential.EncodeToJSON());
+    });
+
+    it('Should be able to create, sign and verify a Verifiable Credential' , function() {
+        VCProof = proofMethod({'issuer' : IssuerDIDDocument, 'issuerKeyId' : "keys-1"});
+        VCProof.Sign(credential.EncodeToJSON());
+        verifiableCredential = VerifiableCredential.Create(credential, VCProof);
+        expect(verifiableCredential.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+    });
+
+    it('Should be able to Encode / Decode a Verifiable Credential and still verify', async function() {
+        this.timeout(80000);
+        await delay(30000);
+        let proofParameters : ProofParameters = await DecodeProofDocument(verifiableCredential.EncodeToJSON().proof, provider);
+        let importedVerifiableCredential : VerifiableCredential = VerifiableCredential.DecodeFromJSON(verifiableCredential.EncodeToJSON(), proofParameters);
+        expect(importedVerifiableCredential.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+        expect(importedVerifiableCredential.EncodeToJSON()).to.deep.equal(verifiableCredential.EncodeToJSON());
+    });
+
+    it('Should test all Verification Error codes for Verifiable Credentials', function() {
+        
+    });
+
+    it('Should be able to create a presentation from a Verifiable Credential', function() {
+        presentation = Presentation.Create([verifiableCredential]);
+        expect(presentation.EncodeToJSON().verifiableCredential[0]).to.deep.equal(verifiableCredential.EncodeToJSON());
+    });
+
+    it('Should be able to Encode / Decode a presentation to be the same', async function() {
+        let importPresentation : Presentation = await Presentation.DecodeFromJSON(presentation.EncodeToJSON(), provider);
+        expect(importPresentation.EncodeToJSON()).to.deep.equal(presentation.EncodeToJSON());
+    });
+
+    it('Should be able to create, sign and verify the Verifiable Presentation', function() {
+        presentationProof = proofMethod({'issuer' : SubjectDIDDocument, 'issuerKeyId' : "keys-1", challengeNonce : "123"});
+        presentationProof.Sign(presentation.EncodeToJSON());
+        verifiablePresentation = VerifiablePresentation.Create(presentation, presentationProof);
+        expect(verifiablePresentation.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+    });
+
+    //verifiablePresentation Shouldn't this be enough to integrate into VerifiableObject and do DecodeProofDocument?
+    it('Should be able to Encode / Decode a Verifiable Presentation and still verify', async function() {
+        let proofParameters : ProofParameters = await DecodeProofDocument(verifiablePresentation.EncodeToJSON().proof, provider);
+        let importVerifiablePresentation : VerifiablePresentation = await VerifiablePresentation.DecodeFromJSON(verifiablePresentation.EncodeToJSON(), provider, proofParameters);
+        expect(importVerifiablePresentation.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+        expect(importVerifiablePresentation.EncodeToJSON()).to.deep.equal(verifiablePresentation.EncodeToJSON());
+    });
+
+    it('Should test all Verification Error codes for Verifiable Presentation', function() {
+
+    });
+
+    let DIDAuth : VerifiablePresentation;
+    it('Should create a DID Authentication Verifiable Presentation', function() {
+        const DIDAuthVC = SignDIDAuthentication(SubjectDIDDocument, "keys-1", GenerateSeed(12));
+        const presentation = Presentation.Create([DIDAuthVC]);
+        const presentationProof = BuildECDSAProof({issuer:SubjectDIDDocument, issuerKeyId:"keys-1", challengeNonce:GenerateSeed(12)});
+        presentationProof.Sign(presentation.EncodeToJSON());
+        DIDAuth = VerifiablePresentation.Create(presentation, presentationProof);
+        SchemaManager.GetInstance().GetSchema("DIDAuthenticationCredential").AddTrustedDID(SubjectDIDDocument.GetDID());
+        expect(DIDAuth.Verify()).to.deep.equal(VerificationErrorCodes.SUCCES);
+        SchemaManager.GetInstance().GetSchema("DIDAuthenticationCredential").RemoveTrustedDID(SubjectDIDDocument.GetDID());
+        expect(DIDAuth.GetVerifiedTypes()).to.deep.equal(["DIDAuthenticationCredential"]);
+    });
+
+    it('Should be able to verify an imported DID Authentication', async function() {
+        expect(await VerifyDIDAuthentication(DIDAuth.EncodeToJSON(), provider)).to.deep.equal(VerificationErrorCodes.SUCCES);
+    });
+});
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}

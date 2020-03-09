a0e73d928f180b968a0a7b8ecdbc90ffdb76d8f4
LisaSchneider94 <lisa.schneider@iota.org>
2020-03-09 18:24:49 +0100

    feat: Hashwrapper for Buffer

--- src/Encryption/Hash.ts
+++ src/Encryption/Hash.ts
@@ -5,3 +5,9 @@ export function Hash(message : string) : string {
     Hasher.update(message);
     return Hasher.digest('base64');
 }
+
+export function HashWrapper(message : string | Buffer ) : Buffer {
+    message = message.toString('hex')
+    const hash = Hash(message)
+    return Buffer.from(hash, 'base64')
+}

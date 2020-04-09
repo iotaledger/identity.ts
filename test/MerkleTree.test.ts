b1508ff0d2e40df3751ff755975c1ebf0bb09432
LisaSchneider94 <lisa.schneider@iota.org>
2020-04-09 17:52:38 +0200

    testing

--- test/MerkleTree.test.ts
+++ test/MerkleTree.test.ts
@@ -2,112 +2,91 @@ import 'mocha';
 import { assert } from 'chai';
 import { HashWrapper, Hash } from '../src/Encryption/Hash'
 import { MerkleTree } from '../src/Encryption/MerkleTree';
-import { hashLeaves, concatHash, concatLetters, hashProof, verify } from '../src/Helpers/MerkleTreeHelper'
 
 
-describe('merkle', function () {
-  it('should handle the base case: [A]', function () {
-    const leaves = ['A'];
-    const merkleTree = new MerkleTree(leaves, concatLetters);
+describe('merkle proof for uneven tree', function () {
+  let leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  const hashTree = new MerkleTree(leaves);
 
-    assert.equal(merkleTree.GetRoot(), "A");
-  });
+ let root = hashTree.GetRoot().toString('hex')
+ const rootArray: string[] = [root , root]
 
-  it('should create a root from two leaves: [A,B]', function () {
-    const leaves = ['A', 'B'];
-    const merkleTree = new MerkleTree(leaves, concatLetters);
-    assert.equal(merkleTree.GetRoot(), "Hash(A + B)");
-  });
+  describe('for each leaf', function () {
+    leaves.forEach((leaf, i) => {
+      it(`should return a proof that calculates the root from leaf ${leaf}`, function () {
+       if(i <= 8){
 
-  it('should create a root from four leaves: [A,B,C,D]', function () {
-    const leaves = ['A', 'B', 'C', 'D'];
-    const merkleTree = new MerkleTree(leaves, concatLetters);
-    assert.equal(merkleTree.GetRoot(), "Hash(Hash(A + B) + Hash(C + D))");
-  });
-
-  it('should create a root from eight leaves: [A,B,C,D,E,F,G,H]', function () {
-    const leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    const merkleTree = new MerkleTree(leaves, concatLetters);
-    assert.equal(merkleTree.GetRoot(), "Hash(Hash(Hash(A + B) + Hash(C + D)) + Hash(Hash(E + F) + Hash(G + H)))");
+        const hashedProof = hashTree.getRootFromLeaf([i, i+1]);
+      
+        assert.deepEqual(hashedProof, rootArray);
+       }
+      });
+    });
   });
 });
 
+describe('merkle proof for even tree', function () {
+  let leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+  const hashTree = new MerkleTree(leaves);
 
-
-
-describe('merkle proof', function () {
-  let leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-  //const root = 'eb100814abc896ab18bcf6c37b6550eeadeae0c312532286a4cf4be132ace526';
-  const root = 'b8ca1ce5be42832ce9d29da4285a8cd0b2dd857e3e9c21e2e150305edd17eeba';
-  const hashTree = new MerkleTree(hashLeaves(leaves), concatHash);
-  const lettersTree = new MerkleTree(leaves.map(HashWrapper), concatLetters);
+ let root = hashTree.GetRoot().toString('hex')
+ const rootArray: string[] = [root , root]
 
   describe('for each leaf', function () {
     leaves.forEach((leaf, i) => {
       it(`should return a proof that calculates the root from leaf ${leaves[i]}`, function () {
-        const proof = hashTree.GetProof(i);
-        const hashedProof = hashProof(leaf, proof).toString('hex');
+       if(i <= 7){
 
-        if (hashedProof !== root) {
-          const lettersProof = lettersTree.GetProof(i);
-          console.log(
-            "The resulting hash of your proof is wrong. \n" +
-            `We were expecting: ${root} \n` +
-            `We recieved: ${hashedProof} \n` +
-            `In ${leaves.join('')} Merkle tree, the proof of ${leaves[i]} you gave us is: \n` +
-            `${JSON.stringify(lettersProof, null, 2)}`
-          );
-        }
-        assert.equal(hashedProof, root);
+        const hashedProof = hashTree.getRootFromLeaf([i, i+1]);
+      
+        assert.deepEqual(hashedProof, rootArray);
+       }
       });
     });
   });
 });
+
 
 
 describe('partial revealing', function () {
-  let leaves: any = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
   const secretAttribute1 = HashWrapper('I')
   const secretAttribute2 = HashWrapper('J')
-  leaves.push(secretAttribute1)
-  leaves.push(secretAttribute2)
-  //const root = 'eb100814abc896ab18bcf6c37b6550eeadeae0c312532286a4cf4be132ace526';
-  const root = 'b8ca1ce5be42832ce9d29da4285a8cd0b2dd857e3e9c21e2e150305edd17eeba';
-  const hashTree = new MerkleTree(hashLeaves(leaves), concatHash);
 
+  let clearLeaves: any = ['A', 'B', 'C', 'D', 'E', ];
+  let hashedLeaves: any = [ secretAttribute1, secretAttribute2]
+  const position: number[] = [ 1 , 4 ]
 
-  //TO DO TYPING OF HASHPROOF AND ADD TO CLASS 
+  const hashTree = new MerkleTree(clearLeaves, hashedLeaves, position);
+
+  let root = hashTree.GetRoot().toString('hex')
+  const rootArray: string[] = [root , root]
+
   describe('for each leaf', function () {
-    leaves.forEach((leaf: any, i: number) => {
-      it(`should return a proof that calculates the root from leaf ${leaves[i]}`, function () {
-        const proof = hashTree.GetProof(i);
-        console.log("proof", proof)
-        const hashedProof = hashProof(leaf, proof).toString('hex');
-
-        if (hashedProof !== root) {
-          console.log("not true")
-        }
-        assert.equal(hashedProof, root);
+    [0,1,2,3,4,5,6].forEach(( i: number) => {
+      it(`should return a proof that calculates the root from leaf`, function () {
+       if(i <= 5){
+        const hashedProof = hashTree.getRootFromLeaf([i, i+1]);
+        assert.deepEqual(hashedProof, rootArray);
+       }
       });
-    });
-  });
+   });
+ });
 });
-
-
 
 
 describe('merkle proof verification', function () {
   describe('a given merkle tree', function () {
     const leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
-    const root = "Hash(Hash(Hash(Hash(A + B) + Hash(C + D)) + Hash(Hash(E + F) + Hash(G + H))) + Hash(Hash(I + J) + K))";
-    const tree = new MerkleTree(leaves, concatLetters);
+    const tree = new MerkleTree(leaves);
+    const root = tree.GetRoot().toString('hex')
 
     describe('untampered proofs', function () {
       leaves.forEach((_, i) => {
         it(`should verify the proof for leaf index ${i}`, function () {
-          const proof = tree.GetProof(i);
-          const verification = verify(proof, leaves[i], root, concatLetters)
-          assert.equal(verification, true);
+          const proof = tree.GetHashesForProofComputation([i]);
+          const verification = tree.verify(proof, [leaves[i]], [root])
+          assert.deepEqual(verification, [ true ]);
         });
       });
     });
@@ -115,34 +94,35 @@ describe('merkle proof verification', function () {
     describe('tampered proofs', function () {
       describe('verifying a different node with a proof', function () {
         it('should not verify the proof', function () {
-          let proof = tree.GetProof(2);
-          assert.equal(verify(proof, leaves[3], root, concatLetters), false);
+          let proof = tree.GetHashesForProofComputation([2]);
+          assert.deepEqual(tree.verify(proof, [leaves[3]], [root]), [ false ]);
         });
       });
 
       describe('verifying a different root', function () {
         it('should not verify the proof', function () {
-          let proof = tree.GetProof(2);
+          let proof = tree.GetHashesForProofComputation([2]);
           const badRoot = "Hash(Hash(Hash(Hash(A + C) + Hash(C + D)) + Hash(Hash(E + F) + Hash(G + H))) + Hash(Hash(I + J) + K))";
-          assert.equal(verify(proof, leaves[2], badRoot, concatLetters), false);
+          assert.deepEqual(tree.verify(proof, [leaves[2]], [badRoot]), [ false ]);
         });
       });
 
       describe('flipping a nodes position', function () {
         it('should not verify the proof', function () {
-          let proof = tree.GetProof(3);
-          proof[1].left = !proof[1].left;
-          assert.equal(verify(proof, leaves[3], root, concatLetters), false);
+          let proof = tree.GetHashesForProofComputation([3]);
+          proof[0][1].left = !proof[0][1].left;
+          assert.deepEqual(tree.verify(proof, [leaves[3]], [root]), [ false ]);
         });
       });
 
       describe('editing a hash', function () {
         it('should not verify the proof', function () {
-          let proof = tree.GetProof(5);
-          proof[2].data = "Q";
-          assert.equal(verify(proof, leaves[5], root, concatLetters), false);
+          let proof = tree.GetHashesForProofComputation([5]);
+          proof[0][2].data = "Q";
+          assert.deepEqual(tree.verify(proof, [leaves[5]], [root]), [ false ]);
         });
       });
     });
   });
 });
+
